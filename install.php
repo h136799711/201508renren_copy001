8b2e143133a7ce62d701ac2f566330b5c28039b4
h136799711 <hebiduhebi@126.com>
2015-06-29 20:01:25 +0800

    UPDATE install.php

--- install.php
+++ install.php
@@ -9,9 +9,9 @@
 
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
+define("PROJECT_NAME","20150606renren");
 define('BOYE_SYS_NAME',true);
 
-//$_GET['m'] = 'Install';
 define('BIND_MODULE','Install');
 /**
  * 系统调试设置
@@ -25,13 +25,12 @@ define ( 'APP_DEBUG', true );
  */
 define ( 'APP_PATH', './Application/' );
 
-/**
- * 缓存目录设置
- * 此目录必须可写，建议移动到非WEB目录
- */
-define ( 'RUNTIME_PATH', './Runtime/' );
 
-/**
- * 引入核心入口
- */
-require './Core/ThinkPHP.php';
+
+// 运行时文件
+define("RUNTIME_PATH","../../Runtime/".PROJECT_NAME."/");
+
+// 框架目录
+define("THINK_PATH",realpath("../../thinkphp/thinkphp_clone/").'/');
+// 加载
+require "../../thinkphp/thinkphp_clone/ThinkPHP.php";
