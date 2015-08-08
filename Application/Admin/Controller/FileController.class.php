89527ec3b7e038e1add2cea0c911c6adc287d7d2
zcs1357520721 <1357520721@qq.com>
2015-08-08 17:47:35 +0800

    部分修复

--- Application/Admin/Controller/FileController.class.php
+++ Application/Admin/Controller/FileController.class.php
@@ -141,5 +141,36 @@ class FileController extends AdminController{
         }
 
     }
+
+
+    /**
+     * 上传文件
+     */
+    public function uploadfile(){
+        dump();
+    }
+
+    /**
+     * 图片上传补做
+     */
+    public function del(){
+        $imgIds=I("imgIds",-1);
+        if($imgIds!=-1){
+            $map=array(
+                'id'=>array(
+                    'in',$imgIds
+                )
+            );
+            $result=apiCall(UserPictureApi::QUERY_NO_PAGING,array($map));
+
+            /*foreach($result['info'] as $v){
+                unlink($v['path']);
+            }*/
+            $result=apiCall(UserPictureApi::DELETE,array($map));
+            if($result['status']){
+                $this->success('删除成功');
+            }
+        }
+    }
 	
 }
